661ef6bc01a2548b2f96ddce28dfbc8805ce08fc
algo_practice <algdsapractice@gmail.com>
2023-01-28 08:30:28 +0530

    Recursion Path Problems

---
+++ src/main/java/Recursion/sequence/MazePath.java
@@ -0,0 +1,53 @@
+package Recursion.sequence;
+
+import java.util.ArrayList;
+
+public class MazePath {
+
+    public static void main(String[] args) {
+        int destinationRow=3;
+        int destinationColumn=3;
+        int sourceRow=1;
+        int sourceColumn=1;
+
+        ArrayList<String> paths = getAllPossibleMazePaths(sourceRow,sourceColumn,destinationRow,destinationColumn);
+        System.out.println(paths);
+    }
+
+    private static ArrayList<String> getAllPossibleMazePaths(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn) {
+        ArrayList<String> allPossiblePaths = new ArrayList<>();
+        ArrayList<String> horizontalPaths= new ArrayList<>();
+        ArrayList<String> verticalPaths=new ArrayList<>();
+
+        if(sourceRow==destinationRow && sourceColumn==destinationColumn){
+            allPossiblePaths.add("");
+            return  allPossiblePaths;
+        }
+
+        if(sourceColumn<destinationColumn){
+            horizontalPaths=getAllPossibleMazePaths(sourceRow,sourceColumn+1,destinationRow,destinationColumn);// horizontal movement
+        }
+        if(sourceRow<destinationRow){
+            verticalPaths=getAllPossibleMazePaths(sourceRow+1,sourceColumn,destinationRow,destinationColumn);// vertical movement
+
+        }
+
+        for (String horizontalPath:horizontalPaths) {
+            allPossiblePaths.add("h"+horizontalPath);
+        }
+
+        for (String verticalPath:verticalPaths) {
+            allPossiblePaths.add("v"+verticalPath);
+        }
+
+
+        return  allPossiblePaths;
+
+
+    }
+
+
+
+
+
+}
---
+++ src/main/java/Recursion/sequence/StairCasePath.java
@@ -0,0 +1,43 @@
+package Recursion.sequence;
+
+import java.util.ArrayList;
+
+public class StairCasePath {
+
+    public static void main(String[] args) {
+
+        int n=5;
+        ArrayList<String> paths = getPossibleStairCasePath(n);
+        System.out.println(paths);
+    }
+
+    private static ArrayList<String> getPossibleStairCasePath(int n) {
+        ArrayList<String>  allThePaths= new ArrayList<>();
+
+        if(n==0){
+            allThePaths.add("");
+            return allThePaths;
+        } else if(n<0){
+
+            return allThePaths;
+        }
+
+
+        ArrayList<String> paths1=getPossibleStairCasePath(n-1);
+        ArrayList<String> paths2=getPossibleStairCasePath(n-2);
+        ArrayList<String> paths3=getPossibleStairCasePath(n-3);
+
+        for (String path1:paths1) {
+            allThePaths.add(1+path1);
+        }
+
+        for (String path2:paths2) {
+            allThePaths.add(2+path2);
+        }
+
+        for (String path3:paths3) {
+            allThePaths.add(3+path3);
+        }
+        return allThePaths;
+    }
+}
