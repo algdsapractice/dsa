b20972ddca1ff5b414d03ba846a5da6f62c72082
algo_practice <algdsapractice@gmail.com>
2023-02-13 21:17:39 +0530

    Flood Fill Solved

---
+++ src/main/java/Recursion/floodfill/FloodFill.java
@@ -0,0 +1,43 @@
+package Recursion.floodfill;
+
+import java.util.Arrays;
+
+public class FloodFill {
+
+    public static void main(String[] args) {
+        int[][] image = {
+                {1,1,1},
+                {1,1,0},
+                {1,0,1}
+        };
+
+        int sr =1, sc=1, color=2;
+
+        int [][] result= floodFill(image,sr,sc,color);
+
+        System.out.println(Arrays.deepToString(result));
+
+    }
+
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+
+
+        if (image[sr][sc] != color) {
+            floodFill(image, sr, sc, color, image[sr][sc]);
+        }
+        return image;
+
+    }
+
+    private static void floodFill(int[][] image, int sr, int sc, int oldColor, int color) {
+        if(sr>=image.length || sc>=image[0].length || sr<0 || sc<0 || image[sr][sc]!=color){
+            return;
+        }
+
+        image[sr][sc]=oldColor;
+        floodFill(image,sr+1,sc,oldColor,color);// top
+        floodFill(image,sr,sc-1,oldColor,color); // left
+        floodFill(image,sr-1,sc,oldColor,color); // down
+        floodFill(image,sr,sc+1,oldColor,color); // right
+    }
+}
