74f3d1f0a5c6fb71c055ba609a8940639dacc0f0
algo_practice <algdsapractice@gmail.com>
2023-02-22 07:52:25 +0530

    Sequence of Non Decreasing

---
+++ src/main/java/Recursion/subset/NonDecreasingSubSeq.java
@@ -0,0 +1,50 @@
+package Recursion.subset;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+//https://leetcode.com/problems/non-decreasing-subsequences/
+public class NonDecreasingSubSeq {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {4,6,7,7};
+        List<List<Integer>> result = findSubSequence(nums);
+        System.out.println(result);
+    }
+
+    private static List<List<Integer>> findSubSequence(int[] nums) {
+        Set<List<Integer>> result = new HashSet<List<Integer>>();
+        List<Integer> bag = new ArrayList<>();
+        int index=0;
+        findSubSequence(nums,index,bag,result);
+
+        return new ArrayList<>(result);
+
+    }
+
+    private static void findSubSequence(int[] nums, int index, List<Integer> bag, Set<List<Integer>> result) {
+
+        if(nums.length==index){
+            if(bag.size()>=2){
+                result.add(new ArrayList<>(bag));
+            }
+            return ;
+        }
+
+        if(bag.isEmpty() || bag.get(index-1)<nums[index]){
+
+            bag.add(nums[index]);
+            findSubSequence(nums,index+1,bag,result);
+            bag.remove(bag.size()-1);
+
+        }
+
+        findSubSequence(nums,index+1,bag,result);
+
+
+    }
+}
